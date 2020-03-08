2985bebe2f9b3a21fc68e0e4dabfa2b9df1dbecf
OldProject1 <47574197+OldProject1@users.noreply.github.com>
2020-03-08 13:22:39 +0300

    Create extension methods.cs

---
+++ extension methods.cs
@@ -0,0 +1,26 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "Привет мир";
+        char c = 'и';
+        int i = s.CharCount(c);
+        Console.WriteLine(i);
+ 
+        Console.Read();
+    }
+}
+ 
+public static class StringExtension
+{
+    public static int CharCount(this string str, char c)
+    {
+        int counter = 0;
+        for (int i = 0; i<str.Length; i++)
+        {
+            if (str[i] == c)
+                counter++;
+        }
+        return counter;
+    }
+} 
